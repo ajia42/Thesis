52e2794e7633bd43670f81d8b56ef503bf2b0fc7
ajia <ajiathavisay3151@gmail.com>
2025-05-21 21:50:47 +0700

    created appointment_management page

--- src/ai/service_type_managment.php
+++ src/ai/service_type_managment.php
@@ -182,7 +182,7 @@ if (!$is_search && empty($search_results)) {
                         </svg>
                         Staff</a></li>
 
-                <li><a href="#">
+                <li><a href="appointment_management.php">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
                             <line x1="16" y1="2" x2="16" y2="6"></line>
